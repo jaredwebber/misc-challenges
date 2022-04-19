520f65cb54d9076026aba83f4baa60089c7d79ee
Jared Webber <jaredwebberdev@gmail.com>
2022-04-18 20:52:00 -0500

    back at it

--- leetcode/README.md
+++ leetcode/README.md
@@ -1,6 +1,6 @@
 # leetcode
 
-Generally un-optimized leetcode problem solutions
-[Blind75](https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions)
-
-[LC Patterns Problem Set](https://seanprashad.com/leetcode-patterns/)
+Generally un-optimized leetcode problem solutions  
+[Blind75](https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions)  
+[Alt Blind75](https://leetcode.com/list/xi4ci4ig/)  
+[LC Patterns Problem Set](https://seanprashad.com/leetcode-patterns/)  
---
+++ leetcode/insert-interval.java
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/insert-interval/
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int[][] outArrays = null;
+        ArrayList<int[]> newIntervals = new ArrayList<int[]>();
+        int index = 0;
+                
+        while(index < intervals.length){
+            if(newInterval != null && (newInterval[0] <= intervals[index][1] || newInterval[0]<=intervals[index][0])){
+                while(index < intervals.length && (newInterval[1] >= intervals[index][0] || newInterval[1]>=intervals[index][0])){
+                    newInterval[0] = Math.min(newInterval[0], intervals[index][0]);
+                    newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
+                    index++;
+                }
+                newIntervals.add(newInterval);
+                newInterval = null;
+            }else{
+                newIntervals.add(intervals[index]);
+                index++;
+            }
+        }
+        
+        if(newInterval != null || intervals.length == 0) newIntervals.add(newInterval);     
+        
+        outArrays = new int[newIntervals.size()][2];
+        for(int i = 0; i<newIntervals.size(); i++){
+            outArrays[i] = newIntervals.get(i);
+        }
+        
+        return outArrays;
+    }
+}
