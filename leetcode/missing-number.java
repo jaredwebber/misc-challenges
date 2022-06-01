1ece5b84f8a503c4446ad8377f1fd41952b48dfd
Jared Webber <jaredwebberdev@gmail.com>
2022-05-31 19:01:12 -0500

    dump

--- leetcode/missing-number.java
+++ leetcode/missing-number.java
@@ -2,11 +2,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int sum = 0;
-        int expected = nums.length;
+        int expected = 0;
         for(int i = 0; i<nums.length; i++){
-            expected+=i;
+            expected+=i+1;
             sum+=nums[i];
         }
-        return expected-sum;
+        return expected - sum;
     }
 }
