f6f25452a429af5f136c413642f1cc4337192682
Jared Webber <jaredwebberdev@gmail.com>
2022-10-08 10:55:54 -0700

    bump

--- leetcode/unique-paths.java
+++ leetcode/unique-paths.java
@@ -1,7 +1,24 @@
 //https://leetcode.com/problems/unique-paths/
 
 class Solution {
+    // More space efficient solution
     public int uniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+        
+        grid[0][0] = 1;
+        
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i > 0) grid[i][j] += grid[i-1][j];
+                if(j > 0) grid[i][j] += grid[i][j-1];
+            }
+        }
+        
+        return grid[m-1][n-1];
+    }
+
+    // Approach without bounds checking
+    public int uniquePathsNoBounds(int m, int n) {
         int[][] grid = new int[m+1][n+1];
         grid[m][n-1] = 1;
         
