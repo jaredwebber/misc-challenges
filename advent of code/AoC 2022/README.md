e46e5b2883d82e512198f692817cff6b510b5e6b
Jared Webber <jaredwebberdev@gmail.com>
2022-12-01 12:24:28 -0800

    re-structure

---
+++ advent of code/AoC 2022/README.md
@@ -0,0 +1,3 @@
+# advent of code 2022
+
+Generally unoptimized Advent of Code 2022 solutions
---
+++ advent of code/AoC 2022/day_one/part_two.py
@@ -0,0 +1,38 @@
+"""
+--- Part Two ---
+
+By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
+
+To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
+
+In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
+
+Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+
+"""
+
+file = open("advent of code/AoC 2022/day_one/input.txt", "r")
+
+vals = [0, 0, 0]
+count = 0
+line = " "
+
+while line:
+    line = file.readline()
+    if line == "\n" or line == "":
+        if count > vals[0]:
+            vals[2] = vals[1]
+            vals[1] = vals[0]
+            vals[0] = count
+        elif count > vals[1]:
+            vals[2] = vals[1]
+            vals[1] = count
+        elif count > vals[2]:
+            vals[2] = count
+        count = 0
+    else:
+        count += int(line)
+
+print(sum(vals))
+
+file.close()
