920c21c04b89e67cfd339ea87b360e58a2e805ca
Jared Webber <jaredwebberdev@gmail.com>
2022-12-08 23:35:18 -0800

    day 9

---
+++ advent of code/AoC 2022/day_9/part_one.py
@@ -0,0 +1,68 @@
+if __name__ == "__main__":
+
+    def update_head(dir: str) -> None:
+        if dir == "U":
+            head_coords[1] += 1
+        elif dir == "R":
+            head_coords[0] += 1
+        elif dir == "D":
+            head_coords[1] -= 1
+        elif dir == "L":
+            head_coords[0] -= 1
+
+    def update_tail() -> None:
+        if head_coords == tail_coords:
+            return
+        if head_coords[0] == tail_coords[0]:
+            # check above and below distances
+            if head_coords[1] - 2 == tail_coords[1]:
+                tail_coords[1] += 1
+            elif head_coords[1] + 2 == tail_coords[1]:
+                tail_coords[1] -= 1
+        elif head_coords[1] == tail_coords[1]:
+            # check left and right distances
+            if head_coords[0] - 2 == tail_coords[0]:
+                tail_coords[0] += 1
+            elif head_coords[0] + 2 == tail_coords[0]:
+                tail_coords[0] -= 1
+        elif (
+            abs(head_coords[0] - tail_coords[0]) > 1
+            or abs(head_coords[1] - tail_coords[1]) > 1
+        ):
+            # bottom left diagonal
+            if tail_coords[0] < head_coords[0] and tail_coords[1] < head_coords[1]:
+                tail_coords[0] += 1
+                tail_coords[1] += 1
+            # bottom right diagonal
+            elif tail_coords[0] > head_coords[0] and tail_coords[1] < head_coords[1]:
+                tail_coords[0] -= 1
+                tail_coords[1] += 1
+            # top right diagonal
+            elif tail_coords[0] > head_coords[0] and tail_coords[1] > head_coords[1]:
+                tail_coords[0] -= 1
+                tail_coords[1] -= 1
+            # top left diagonal
+            elif tail_coords[0] < head_coords[0] and tail_coords[1] > head_coords[1]:
+                tail_coords[0] += 1
+                tail_coords[1] -= 1
+
+        tail_visited.add(tuple(tail_coords))
+
+    file = open("day_9/input.txt", "r")
+
+    tail_visited = set()  # set of coordinates
+    head_coords = [0, 0]
+    tail_coords = [0, 0]
+    tail_visited.add(tuple(tail_coords))
+
+    for line in file:
+        split_line = line.strip().split(" ")
+        dir = split_line[0]
+        steps = int(split_line[1])
+        for step in range(steps):
+            update_head(dir)
+            update_tail()
+
+    print(len(tail_visited))
+
+    file.close()
---
+++ advent of code/AoC 2022/day_9/part_two.py
@@ -0,0 +1,89 @@
+if __name__ == "__main__":
+
+    def update_head(dir: str) -> None:
+        if dir == "U":
+            rope[0][1] += 1
+        elif dir == "R":
+            rope[0][0] += 1
+        elif dir == "D":
+            rope[0][1] -= 1
+        elif dir == "L":
+            rope[0][0] -= 1
+
+    def update_rope() -> None:
+        for knot in range(1, 10):
+            if rope[knot - 1][0] == rope[knot][0]:
+                # check above and below distances
+                if rope[knot - 1][1] - 2 == rope[knot][1]:
+                    rope[knot][1] += 1
+                elif rope[knot - 1][1] + 2 == rope[knot][1]:
+                    rope[knot][1] -= 1
+            elif rope[knot - 1][1] == rope[knot][1]:
+                # check left and right distances
+                if rope[knot - 1][0] - 2 == rope[knot][0]:
+                    rope[knot][0] += 1
+                elif rope[knot - 1][0] + 2 == rope[knot][0]:
+                    rope[knot][0] -= 1
+            elif (
+                abs(rope[knot - 1][0] - rope[knot][0]) > 1
+                or abs(rope[knot - 1][1] - rope[knot][1]) > 1
+            ):
+                # bottom left diagonal
+                if (
+                    rope[knot][0] < rope[knot - 1][0]
+                    and rope[knot][1] < rope[knot - 1][1]
+                ):
+                    rope[knot][0] += 1
+                    rope[knot][1] += 1
+                # bottom right diagonal
+                elif (
+                    rope[knot][0] > rope[knot - 1][0]
+                    and rope[knot][1] < rope[knot - 1][1]
+                ):
+                    rope[knot][0] -= 1
+                    rope[knot][1] += 1
+                # top right diagonal
+                elif (
+                    rope[knot][0] > rope[knot - 1][0]
+                    and rope[knot][1] > rope[knot - 1][1]
+                ):
+                    rope[knot][0] -= 1
+                    rope[knot][1] -= 1
+                # top left diagonal
+                elif (
+                    rope[knot][0] < rope[knot - 1][0]
+                    and rope[knot][1] > rope[knot - 1][1]
+                ):
+                    rope[knot][0] += 1
+                    rope[knot][1] -= 1
+
+        tail_visited.add(tuple(rope[9]))
+
+    file = open("day_9/input.txt", "r")
+
+    tail_visited = set()  # set of coordinates
+    rope = [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+    ]
+    tail_visited.add(tuple(rope[9]))
+
+    for line in file:
+        split_line = line.strip().split(" ")
+        dir = split_line[0]
+        steps = int(split_line[1])
+        for step in range(steps):
+            update_head(dir)
+            update_rope()
+
+    print(len(tail_visited))
+
+    file.close()
