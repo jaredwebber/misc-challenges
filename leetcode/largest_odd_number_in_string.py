e25e2b68593704c20f3897494ee62178f60875ed
Jared Webber <jaredwebberdev@gmail.com>
2023-12-07 08:32:19 -0600

    daily

---
+++ leetcode/largest_odd_number_in_string.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/largest-odd-number-in-string
+
+
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 != 0:
+                return num[: i + 1]
+        return ""
