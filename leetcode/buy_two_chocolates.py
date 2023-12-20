4a9364c9c88691d85d9ddf2f577da2bb7b8b86d8
Jared Webber <jaredwebberdev@gmail.com>
2023-12-20 09:44:32 -0600

    update daily

--- leetcode/buy_two_chocolates.py
+++ leetcode/buy_two_chocolates.py
@@ -12,3 +12,8 @@ class Solution:
                 two_smallest[1] = i
 
         return money if sum(two_smallest) > money else money - sum(two_smallest)
+
+    # shorter approach requiring sorting
+    def buyChoco_alt(self, prices: list[int], money: int) -> int:
+        prices.sort()
+        return money - prices[0] - prices[1] if money - prices[0] - prices[1] >= 0 else money
